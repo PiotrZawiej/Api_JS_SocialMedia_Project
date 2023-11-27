27b3474afead73099ec0836e326eab8c2f060649
PiotrZawiej <piotr.zawieja0x@gmail.com>
2023-11-27 16:19:38 +0100

    Dispaly all posts in API on website

--- ApiCall.js
+++ ApiCall.js
@@ -1,40 +1,58 @@
-let usersID = [];
+const postPromise = fetch("https://jsonplaceholder.typicode.com/posts").then(
+  (response) => response.json()
+);
+const photoPromise = fetch("https://jsonplaceholder.typicode.com/photos").then(
+  (response) => response.json()
+);
 
-// Fetch post data
-const postPromise = fetch("https://jsonplaceholder.typicode.com/posts/1")
-  .then((response) => {
-    return response.json();
-  })
-  .then((posts) => {
-    document.querySelector(".post-title").textContent = posts.title;
-    document.querySelector(".post-body").textContent = posts.body;
-    usersID.push(posts.userId);
-  });
+const userPromise = fetch("https://jsonplaceholder.typicode.com/users").then(
+  (response) => response.json()
+);
 
-// Fetch photo data
-const photoPromise = fetch("https://jsonplaceholder.typicode.com/photos/1")
-  .then((response) => {
-    return response.json();
-  })
-  .then((photos) => {
-    const photoEl = document.querySelector(".photo");
-    photoEl.src = photos.thumbnailUrl;
-    photoEl.alt = photos.title;
-  });
+Promise.all([postPromise, photoPromise, userPromise]).then(
+  ([posts, photos, users]) => {
+    for (let i = 0; i < posts.length; i++) {
+      userID = 0;
+      let post = posts[i];
+      let photo = photos[i];
+      let userIndex = i % users.length;
+      console.log(userIndex);
 
-const userPromise = fetch("https://jsonplaceholder.typicode.com/users/1")
-  .then((response) => {
-    return response.json();
-  })
-  .then((users) => {
-    const userEl = document.querySelector(".user-name");
-    if (usersID[0] == users.id) {
-      userEl.textContent = users.username;
+      let postContainerEl = document.createElement("div");
+      postContainerEl.className = "post-conatainer";
+
+      let userPhotoEl = document.createElement("div");
+      userPhotoEl.className = "photo-user";
+
+      let postPhoto = document.createElement("img");
+      postPhoto.className = "photo";
+      postPhoto.src = photo.thumbnailUrl;
+      postPhoto.alt = photo.title;
+
+      let userNameEl = document.createElement("div");
+      userNameEl.className = "user-name";
+      userNameEl.textContent = users[userIndex].username;
+
+      userPhotoEl.appendChild(postPhoto);
+      userPhotoEl.appendChild(userNameEl);
+
+      let titleBodyEl = document.createElement("div");
+
+      let postTitle = document.createElement("div");
+      postTitle.className = "post-title";
+      postTitle.textContent = post.title;
+
+      let postBody = document.createElement("div");
+      postBody.className = "post-body";
+      postBody.textContent = post.body;
+
+      titleBodyEl.appendChild(postTitle);
+      titleBodyEl.appendChild(postBody);
+
+      postContainerEl.appendChild(userPhotoEl);
+      postContainerEl.appendChild(titleBodyEl);
+
+      document.body.appendChild(postContainerEl);
     }
-  });
-
-// Wait for both promises to resolve
-Promise.all([postPromise, photoPromise]).then(() => {
-  // Now usersID has the data from both fetch requests
-  console.log(usersID);
-});
+  }
+);
