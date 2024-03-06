8968b030d39c70fa326cad90ab19aeeeeeaa3de2
PiotrZawiej <piotr.zawieja0x@gmail.com>
2024-03-06 20:24:54 +0100

    smashed too big functions

--- script/posts.js
+++ script/posts.js
@@ -1,64 +1,37 @@
-import { fetchData } from "./ApiCall.js";
+//posts.js
 
-const targetIpAddress = "desired.ip.address";
+import { fetchData } from "./ApiCall.js";
+import { createCommentsContainer } from "./CreateConatiner.js";
+import { createAvatarElement } from "./CreateConatiner.js";
+import { createTitleBodyElement } from "./CreateConatiner.js";
+import { createFeedbackElement } from "./CreateConatiner.js";
 
 fetchData().then(([posts, photos, users, comments]) => {
-  for (let i = 0; i < 10; i++) {
-    // const randomIndex = Math.floor(Math.random() * posts.length);
+  for (let i = 0; i < 100; i++) {
+    const randomIndex = Math.floor(Math.random() * posts.length);
 
-    let post = posts[i];
-    let photo = photos[i];
-    let userIndex = posts[i].userId;
-    let commentIndex = posts[i].id;
+    let post = posts[randomIndex];
+    let photo = photos[randomIndex]; // Fixed this line
+    let user = users[post.userId - 1]; // Fixed this line
+    let commentIndex = post.id;
 
     let postContainerEl = document.createElement("div");
     postContainerEl.className = "post-conatainer";
 
-    let userPhotoEl = document.createElement("div");
-    userPhotoEl.className = "photo-user";
+    let commentsContainer = createCommentsContainer(); // Moved this line here
 
-    let postPhoto = document.createElement("img");
-    postPhoto.className = "photo";
-    postPhoto.src = photo.thumbnailUrl;
-    postPhoto.alt = photo.title;
+    while (commentIndex === comments.postId) {
+      if (commentIndex === comments.postId) {
+        commentsContainer.textContent = comments[commentIndex - 1].body;
+      } else {
+        commentIndex += 1;
+      }
+    }
 
-    let userNameEl = document.createElement("div");
-    userNameEl.className = "user-name";
-    userNameEl.textContent = users[userIndex - 1].username;
-
-    userPhotoEl.appendChild(postPhoto);
-    userPhotoEl.appendChild(userNameEl);
-
-    let titleBodyEl = document.createElement("div");
-
-    let postTitle = document.createElement("div");
-    postTitle.className = "post-title";
-    postTitle.textContent = post.title;
-
-    let postBody = document.createElement("div");
-    postBody.className = "post-body";
-    postBody.textContent = post.body;
-
-    titleBodyEl.appendChild(postTitle);
-    titleBodyEl.appendChild(postBody);
-
-    let feedback = document.createElement("div");
-    feedback.className = "feedback-constainer";
-
-    let commentsLink = document.createElement("div");
-    commentsLink.className = "comments-link";
-    commentsLink.textContent = "comments";
-
-    feedback.appendChild(commentsLink);
-
-    let commentsContainer = document.createElement("div");
-    commentsContainer.className = "commments-container";
-    commentsContainer.textContent = comments[commentIndex - 1].body;
-
-    postContainerEl.appendChild(userPhotoEl);
-    postContainerEl.appendChild(titleBodyEl);
-    postContainerEl.appendChild(feedback);
-    postContainerEl.appendChild(commentsContainer);
+    postContainerEl.appendChild(createAvatarElement(photo, user)); // Fixed this line
+    postContainerEl.appendChild(createTitleBodyElement(post));
+    postContainerEl.appendChild(createFeedbackElement());
+    postContainerEl.appendChild(commentsContainer); // Fixed this line
 
     document.body.appendChild(postContainerEl);
   }
